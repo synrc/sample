4259a9eac7ace5a72ac62ca872c9bce0c3add1b1
Namdak Tonpa <maxim@synrc.com>
2018-11-14 08:26:09 +0200

    cowboy 2.5

--- src/routes.erl
+++ src/routes.erl
@@ -3,12 +3,13 @@
 -include_lib("n2o/include/n2o.hrl").
 -export([init/2, finish/2]).
 
+% cowboy 2.5
 finish(State, Ctx) -> {ok, State, Ctx}.
-init(State, Ctx) ->
-    Path = n2o_cowboy:path(Ctx#cx.req),
+init(State, #cx{req=Req}=Cx) ->
+    #{path := Path} = Req,
     Fix  = route_prefix(Path),
     n2o:info(?MODULE,"Route: ~p~n",[{Fix,Path}]),
-    {ok, State, Ctx#cx{path=Path,module=Fix}}.
+    {ok, State, Cx#cx{path=Path,module=Fix}}.
 
 route_prefix(<<"/ws/",P/binary>>) -> route(P);
 route_prefix(<<"/",P/binary>>) -> route(P);

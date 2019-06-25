495a81a6a109f2792f220c82139c70ed8f1eaae2
Namdak Tonpa <maxim@synrc.com>
2019-06-26 02:51:28 +0300

    SAMPLE 6.6:
    * new KVS
    * MNESIA by default
    * remove DTL
    * index cleanup

--- src/index.erl
+++ src/index.erl
@@ -1,42 +1,39 @@
 -module(index).
 -compile(export_all).
--include_lib("kvs/include/entry.hrl").
 -include_lib("nitro/include/nitro.hrl").
 -include_lib("n2o/include/n2o.hrl").
+-include_lib("kvs/include/cursors.hrl").
 event(init) ->
     Room = n2o:session(room),
-    n2o:reg({topic,Room}),
+    Key = {topic,Room},
+    n2o:reg(Key),
     Sid = (get(context))#cx.session,
-    ?LOG_INFO("Room: ~p~n",[{Room,Sid}]),
     n2o:reg(Sid),
     nitro:clear(history),
     nitro:update(logout, #button{id=logout, body="Logout " ++ n2o:user(), postback=logout}),
-    nitro:update(heading, #h2{id=heading, body=Room}),
+    nitro:update(heading, [#h2{id=heading, body=Room}]),
     nitro:update(upload, #upload{}),
     nitro:update(send, #button{id=send, body= <<"Chat">>, postback=chat, source=[message] }),
-    [ event({client,{E#entry.from,E#entry.media}})
-      || E <- lists:reverse(kvs:entries(kvs:get(feed,{room,Room}),entry,10)) ];
+    [ event(#client{data=E})  || E <- lists:reverse(kvs:feed(Key)) ];
 event(logout) ->
     n2o:user([]),
     nitro:redirect("/app/login.htm");
 event(chat) ->
-    User    = n2o:user(),
-    Room    = n2o:session(room),
-    Message = nitro:q(message),
-    ?LOG_INFO("Chat pressed: ~p~n",[{Room,Message,User}]),
-    kvs:add(#entry{id=kvs:next_id("entry",1),from=n2o:user(),
-                   feed_id={room,Room},media=Message}),
-    n2o:send({topic,Room},#client{data={User,Message}});
-event(#client{data={User,Message}}) ->
+    chat(nitro:q(message));
+event(#client{data={'$msg',_,_,_,User,Message}}) ->
     HTML = nitro:to_list(Message),
     nitro:wire(#jq{target=message,method=[focus,select]}),
-    DTL = #dtl{file="message",app=review,bindings=[{user,User},{color,"gray"},{message,HTML}]},
-    nitro:insert_top(history, nitro:jse(nitro:render(DTL)));
+    nitro:insert_top(history, nitro:render(#message{body=[#author{body=User},nitro:jse(HTML)]}));
 event(#ftp{sid=Sid,filename=Filename,status={event,stop}}=Data) ->
-    ?LOG_INFO("FTP Delivered ~p~n",[Data]),
     Name = hd(lists:reverse(string:tokens(nitro:to_list(Filename),"/"))),
-    erlang:put(message,nitro:render(#link{href=iolist_to_binary(["/app/",Sid,"/",Name]),body=Name})),
-    event(chat);
+    chat(nitro:render(#link{href=iolist_to_binary(["/app/",Sid,"/",Name]),body=Name}));
 event(Event) ->
     ?LOG_INFO("Event: ~p", [Event]),
     ok.
+
+chat(Message) ->
+    Room = n2o:session(room),
+    User = n2o:user(),
+    Msg = {'$msg', kvs:seq([], []), [], [], User, nitro:jse(Message)},
+    kvs:append(Msg,{topic,Room}),
+    n2o:send({topic, Room}, #client{data = Msg}).

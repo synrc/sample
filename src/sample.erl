89bc41fb6e2570c4917fcda4008dcd6842b05061
Namdak Tonpa <maxim@synrc.com>
2020-02-15 17:33:33 +0200

    Update sample.erl

--- src/sample.erl
+++ src/sample.erl
@@ -5,7 +5,7 @@
 main(A)    -> mad:main(A).
 stop(_)    -> ok.
 start()    -> start(normal,[]).
-start(_,_) -> cowboy:start_clear(http,[{port,application:get_env(n2o,port,8002)}],
+start(_,_) -> cowboy:start_clear(http,[{port,application:get_env(n2o,port,8001)}],
                  #{env=>#{dispatch=>n2o_cowboy2:points() }}),
               supervisor:start_link({local,sample},sample,[]).
 init([])   -> kvs:join(), syn:init(), {ok, {{one_for_one, 5, 10}, [] }}.

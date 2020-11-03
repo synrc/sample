8e8044bb40f239063b29e7876dc7d35983f43cf8
Andrii Zadorozhnii <doxtop@synrc.com>
2020-11-03 02:40:41 +0200

    update deps and conf

--- src/sample.erl
+++ src/sample.erl
@@ -6,6 +6,6 @@ main(A)    -> mad:main(A).
 stop(_)    -> ok.
 start()    -> start(normal,[]).
 start(_,_) -> cowboy:start_clear(http,[{port,application:get_env(n2o,port,8001)}],
-                 #{env=>#{dispatch=>n2o_cowboy2:points() }}),
+                 #{env=>#{dispatch=>n2o_cowboy:points() }}),
               supervisor:start_link({local,sample},sample,[]).
-init([])   -> kvs:join(), syn:init(), {ok, {{one_for_one, 5, 10}, [] }}.
+init([])   -> kvs:join(), {ok, {{one_for_one, 5, 10}, [] }}.
